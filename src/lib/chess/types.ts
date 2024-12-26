07281284125e8157f037a77da5a1511e2eb3de7d
thomaslekieffre <thomaslekieffre59.dev@gmail.com>
2024-12-26 20:07:22 +0100

    élo bug

--- src/lib/chess/types.ts
+++ src/lib/chess/types.ts
@@ -1,77 +1,144 @@
 export const PROMOTIONS = {
-  'q': 'queen',
-  'r': 'rook',
-  'b': 'bishop',
-  'n': 'knight'
-}
+  q: "queen",
+  r: "rook",
+  b: "bishop",
+  n: "knight",
+};
 
-export const prom_short = ['q', 'r', 'b', 'n']
-export type PROMOTIONS_SHORT = typeof prom_short[number]
+export const prom_short = ["q", "r", "b", "n"];
+export type PROMOTIONS_SHORT = (typeof prom_short)[number];
 
+export const colors = ["white", "black"] as const;
+export const files = ["a", "b", "c", "d", "e", "f", "g", "h"] as const;
+export const ranks = ["1", "2", "3", "4", "5", "6", "7", "8"] as const;
+export const ranksOpo = ["8", "7", "6", "5", "4", "3", "2", "1"] as const;
+export type MoveExeptType = ["O-O", "O-O-O", "a0"];
 
+export type File = (typeof files)[number];
+export type Rank = (typeof ranks)[number];
+export type CasesList = `${File}${Rank}`;
+export type Field = CasesList | MoveExeptType;
 
-export const colors = ['white', 'black'] as const
-export const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'] as const
-export const ranks = ['1', '2', '3', '4', '5', '6', '7', '8'] as const
-export const ranksOpo = ['8', '7', '6', '5', '4', '3', '2', '1'] as const
-export type MoveExeptType = ['O-O','O-O-O','a0']
+export type GameComment = {
+  comment?: string;
+  colorArrows?: string[];
+  colorFields?: string[];
+  clk?: string;
+  eval?: string;
+};
+export type Color = "w" | "b";
 
-export type File = typeof files[number]
-export type Rank = typeof ranks[number]
-export type CasesList = `${File}${Rank}`
-export type Field = CasesList | MoveExeptType
-
-export type GameComment = { comment?: string, colorArrows?: string[], colorFields?: string[], clk?: string, eval?: string }
-export type Color = 'w' | 'b'
-
-export type ConfigurationTagsValues = "no" | "7r" | "known" | "all"
+export type ConfigurationTagsValues = "no" | "7r" | "known" | "all";
 export type PgnWriterConfiguration = {
-  tags?: ConfigurationTagsValues,
-  notation?: string
-}
+  tags?: ConfigurationTagsValues;
+  notation?: string;
+};
 
-export const fenAbrListe = ['p', 'k', 'q', 'n','b','r','P','K','Q','N','B','R'] as const
-export type fenAbrListeType = typeof fenAbrListe[number]
+export const fenAbrListe = [
+  "p",
+  "k",
+  "q",
+  "n",
+  "b",
+  "r",
+  "P",
+  "K",
+  "Q",
+  "N",
+  "B",
+  "R",
+] as const;
+export type fenAbrListeType = (typeof fenAbrListe)[number];
 export type FenEmptyChar = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8";
 export type FenRowChar = `${fenAbrListeType | FenEmptyChar}`;
 export type FenPiecePlacement = string; // Exemple: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 export type FenActiveColor = "w" | "b"; // Blanc ou noir
-export const tabCastlingRights = ['KQkq','KQk','KQ','KQq','Kkq','Kk','Kq','Qkq','Qk','Qq','K','Q','k','q','-'] as const; // Exemple: "KQkq", "KQ", "kq", ou "-"
-export type FenCastlingRights = 'KQkq'|'KQk'|'KQ'|'KQq'|'Kkq'|'Kk'|'Kq'|'Qkq'|'Qk'|'Qq'|'K'|'Q'|'k'|'q'|'-'
-export type FenEnPassant = "-" | `${"a" | "b" | "c" | "d" | "e" | "f" | "g" | "h"}${"3" | "6"}`; // Exemple: "e3", "d6", ou "-"
+export const tabCastlingRights = [
+  "KQkq",
+  "KQk",
+  "KQ",
+  "KQq",
+  "Kkq",
+  "Kk",
+  "Kq",
+  "Qkq",
+  "Qk",
+  "Qq",
+  "K",
+  "Q",
+  "k",
+  "q",
+  "-",
+] as const; // Exemple: "KQkq", "KQ", "kq", ou "-"
+export type FenCastlingRights =
+  | "KQkq"
+  | "KQk"
+  | "KQ"
+  | "KQq"
+  | "Kkq"
+  | "Kk"
+  | "Kq"
+  | "Qkq"
+  | "Qk"
+  | "Qq"
+  | "K"
+  | "Q"
+  | "k"
+  | "q"
+  | "-";
+export type FenEnPassant =
+  | "-"
+  | `${"a" | "b" | "c" | "d" | "e" | "f" | "g" | "h"}${"3" | "6"}`; // Exemple: "e3", "d6", ou "-"
 export type FenHalfmoveClock = number; // Demi-coups depuis capture/pion
 export type FenFullmoveNumber = number; // Numéro de tour
-export type FenString = `${string} ${FenActiveColor} ${FenCastlingRights} ${FenEnPassant} ${FenHalfmoveClock} ${FenFullmoveNumber}`;
+export type FenString =
+  `${string} ${FenActiveColor} ${FenCastlingRights} ${FenEnPassant} ${FenHalfmoveClock} ${FenFullmoveNumber}`;
 
 export type PgnMove = {
   drawOffer?: boolean;
   // moveNumber?: number,
-  notation: { fig?: string | null, strike?: 'x' | null, col?: string, row?: string, check?: string, ep?: boolean
-  promotion?: string | null, notation: string, disc?: string, drop?: boolean },
-  variations: PgnMove[],
-  nag: string[],
-  commentDiag?: GameComment,
-  commentMove?: string,
-  commentAfter?: string,
-  turn: PieceColor
-  from: CasesList,
-  to: CasesList,
-  fen: FenString|string,
-  index?: number,
-  turnNumber:number,
-  prev?: number,
-  next?: number,
-  variationLevel?: number
-}
+  notation: {
+    fig?: string | null;
+    strike?: "x" | null;
+    col?: string;
+    row?: string;
+    check?: string;
+    ep?: boolean;
+    promotion?: string | null;
+    notation: string;
+    disc?: string;
+    drop?: boolean;
+  };
+  variations: PgnMove[];
+  nag: string[];
+  commentDiag?: GameComment;
+  commentMove?: string;
+  commentAfter?: string;
+  turn: PieceColor;
+  from: CasesList;
+  to: CasesList;
+  fen: FenString | string;
+  index?: number;
+  turnNumber: number;
+  prev?: number;
+  next?: number;
+  variationLevel?: number;
+};
 
-export type SevenRoosterTagKeys = 'Event'|'Site'|'Round'|'White'|'Black'|'Result|Date'
-export type Tags = { string: string }
+export type SevenRoosterTagKeys =
+  | "Event"
+  | "Site"
+  | "Round"
+  | "White"
+  | "Black"
+  | "Result|Date";
+export type Tags = { string: string };
 
 export type PgnGame = {
-  tags?: Tags,
-  gameComment?: GameComment,
-  moves: PgnMove[]
-}
+  tags?: Tags;
+  gameComment?: GameComment;
+  moves: PgnMove[];
+};
 
 export type Position = { x: number; y: number };
 export type PieceColor = "white" | "black";
@@ -83,13 +150,7 @@ export type PieceType =
   | "queen"
   | "king";
 
-export type PieceTypeAbreg = 
-  | "P"
-  | "R"
-  | "N"
-  | "B"
-  | "Q"
-  | "K";
+export type PieceTypeAbreg = "P" | "R" | "N" | "B" | "Q" | "K";
 
 export interface ChessPiece {
   type: PieceType;
@@ -108,12 +169,12 @@ export interface Move {
   promotedTo?: PieceType;
 }
 
-export type drawReason=
-| "stalemate"
-| "insufficient-material"
-| "threefold-repetition"
-| "fifty-moves"
-| "mutual-agreement";
+export type drawReason =
+  | "stalemate"
+  | "insufficient-material"
+  | "threefold-repetition"
+  | "fifty-moves"
+  | "mutual-agreement";
 
 export interface GameState {
   board: (ChessPiece | null)[][];
@@ -122,11 +183,11 @@ export interface GameState {
   isCheckmate: boolean;
   isStalemate: boolean;
   isDraw: boolean;
-  drawReason?:drawReason;
+  drawReason?: drawReason;
   moves: Move[];
   enPassantTarget?: Position;
   moveCount: number;
-  halfMoveCount:number;
+  halfMoveCount: number;
   positions: string[];
   lastPawnMoveOrCapture: number;
   drawOffer?: PieceColor;
@@ -135,33 +196,44 @@ export interface GameState {
     black: number;
   };
   isGameOver: boolean;
-  drawnHasBeenOffered:boolean;
+  drawnHasBeenOffered: boolean;
   winner: PieceColor | null;
-  strMove:PgnMove[];
-  castlingRights:FenCastlingRights;
+  strMove: PgnMove[];
+  castlingRights: FenCastlingRights;
 }
 
 export type playerType = {
-  id:string;
-  color:PieceColor;
-  temp:string;
-  username:string;
-  elo:string;
-}
+  id: string;
+  color: PieceColor;
+  time: string;
+  username: string;
+  elo_stats?: {
+    classique: {
+      bullet?: string;
+      blitz?: string;
+      rapide?: string;
+    };
+    saisonnier?: {
+      bullet?: string;
+      blitz?: string;
+      rapide?: string;
+    };
+  };
+};
 
 export type roomType = {
-  id:string;
-  cadence:string;
-  game_mode:string;
-  status:string;
-  rated:Boolean;
-  players:{
-    player1:playerType;
-    player2:playerType;
-    lastmove:string;
-  }
-  game:PgnMove[];
-  default_pos:string|FenString;
-  createdAt:string;
-  turn:string;
-}
+  id: string;
+  cadence: string;
+  game_mode: string;
+  status: string;
+  rated: boolean;
+  players: {
+    player1: playerType;
+    player2: playerType;
+    lastmove: string;
+  };
+  game: PgnMove[];
+  default_pos: string | FenString;
+  createdAt: string;
+  turn: string;
+};
