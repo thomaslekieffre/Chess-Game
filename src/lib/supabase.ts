c9a5527dc2d66869239f772eeb2040283c66ef13
thomaslekieffre <thomaslekieffre59.dev@gmail.com>
2025-01-10 17:39:16 +0100

    micro fix: .env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,4 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL and Anon Key must be provided");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
